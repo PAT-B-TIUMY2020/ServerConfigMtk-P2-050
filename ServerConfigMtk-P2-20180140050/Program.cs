a22f39ed754f0ecf08de696f69910e2c825b2716
adelina-sfr <62781888+adelina-sfr@users.noreply.github.com>
2020-11-07 14:47:24 +0700

    Add exception

--- ServerConfigMtk-P2-20180140050/Program.cs
+++ ServerConfigMtk-P2-20180140050/Program.cs
@@ -3,13 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.ServiceModel;
+using ServiceMtk_P1_20180140050;
 namespace ServerConfigMtk_P2_20180140050
 {
     class Program
     {
         static void Main(string[] args)
         {
+            ServiceHost hostObj = null;
+            try
+            {
+                hostObj = new ServiceHost(typeof(Matematika));
+                hostObj.Open();
+                Console.WriteLine("Server is Ready!!!");
+                Console.ReadLine();
+                hostObj.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObj = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
         }
     }
 }
